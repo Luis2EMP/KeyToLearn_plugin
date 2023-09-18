e48c8800f39b30e17fc7be3b510ae6abc186d720
Luis Eduardo <89282065+Luis2EMP@users.noreply.github.com>
2023-09-18 03:43:59 -0300

    Add files via upload

---
+++ README.md
@@ -0,0 +1,80 @@
+#KeyToLearn
+<!-- Primeiro nome que botei era StudyKeyFlow, que a abreviação SKF dava pra entender ShortcutKeysFlow tbm, era um nome ok, mas quis trocar, troquei quando estava finalizando esse .MD 02:42 18/09/23-->
+Este plugin em lua para o [Xournal++](https://github.com/xournalpp/xournalpp "Xournal++") adiciona diversas de combinações de teclas que são rapidamente acessíveis com a mão esquerda, principalmente sem a necessidade de teclas modificadoras, com apenas o Shift que não é muito usado como atalho no programa normal.
+
+####Table of contents
+
+<!-- [TOCM]  -->
+<!--Comment in .MD https://gist.github.com/jonikarppinen/47dc8c1d7ab7e911f4c9 -->
+
+[TOC]
+
+##Instalação
+A instalação consiste em colocar os arquivos disponibilizados em suas respectivas pastas.
+
+Com base no recurso [Xournal++ → Guide → File locations](https://xournalpp.github.io/guide/file-locations/ "Xournal++ → Guide → File locations"), será preciso acessar as pastas de [Shared resources](https://xournalpp.github.io/guide/file-locations/#shared-resources-folder "Shared resources") e de [Config](https://xournalpp.github.io/guide/file-locations/#config-folder "Config").
+
+###Plugin
+Na pasta `.\share\xournalpp\plugins` adicione a pasta do plugin **SKeysFlow**.
+
+###Page template
+Na pasta `.\share\xournalpp\ui` adicione o arquivo **pagetemplate.ini** .
+
+###Toolbar, paleta de cores e settings
+Na pasta `.\AppData\Local\xournalpp` adicione o arquivo **palette.gpl**, **settings.xml** e **toolbar.ini**.
+
+####Settings
+Nas settings que eu disponibilizei, além das minha configurações padrões de caneta, tema e outros, também tem mudança do `snapGridSize` para conseguir usar o template da pagina com a grade certa. O padrão dos formatos de pagina `format = graph` é `<property name="snapGridSize" value="14.17"/>` , e esse valor deve ser mudado para `<property name="snapGridSize" value="10"/>`
+
+###Fontes
+As principais fontes usadas são a [Strawberry Muffins](https://www.dafont.com/pt/strawberry-muffins.font?back=theme "Strawberry Muffins") e Times New Roman, normamente usadas em 6 ou 5 pt para ficar do mesmo tamanho da minha escrita.  Na pasta de **fontes** tem algumas outras fontes alternativas que parecem escritas a mão com as imagens delas em uso, acredito que todas contem caracteres com acentos e "ç".
+
+##Recursos
+Para fazer o plugin e todas as outras mudanças de template e configurações foi usado o repositorio do Xournal++ para pesquisar tudo que eu posso alterar e entender como alterar. A seguir tem os links que mais me ajudaram.
+
+###Para o plugin
+- [Guia de plugins.](https://xournalpp.github.io/guide/plugins/plugins/")
+	Guia de plugins da pagina principal do Xournal, que também contem outras informações importantes. 
+
+- [Issue: Configurable shortcuts/keybindings
+](https://github.com/xournalpp/xournalpp/issues/919)
+	Thread com 100 comentarios falando bastante sobre construção de plugins para atalhos
+
+- [ Segmento de código da função `Control::actionPerformed`](https://github.com/xournalpp/xournalpp/blob/c07654780933929a92e9187ad0dc44a80fb04cc7/src/core/control/Control.cpp#L360-L950)
+	Onde é configurado as todas funcões que da pra usar com `app.uiAction`.
+
+- [Lua api para os plugins no xournal](https://github.com/xournalpp/xournalpp/blob/7b6d84956d6bbe8615b2123c64dd0cac80afb81a/src/core/plugin/luapi_application.h)
+	 API do Xournalpp para os plugin. Contem exemplos de uso, como na linha 968.
+
+- [Segmento de código na lua api, linha 1723](https://github.com/xournalpp/xournalpp/blob/7b6d84956d6bbe8615b2123c64dd0cac80afb81a/src/core/plugin/luapi_application.h#L1723-L1750)
+	Contem todas as funções que podem ser usadas.
+
+- [Plugins instalados com o programa](https://github.com/xournalpp/xournalpp/tree/master/plugins)
+	Podem ser usados de referência.
+
+- [CHANGELOG](https://github.com/xournalpp/xournalpp/blob/8caa0bc7cace73c524ae02c1f08475b0c92800c6/CHANGELOG.md?plain=1#L241)
+	Para saber as mudanças e para verificar se há novas funções que podem ser incluídas ou removidas, caso já tenham sido adicionadas ao programa principal.
+
+- [Plugin vi-xournalpp do Raw Bacon](https://github.com/raw-bacon/vi-xournalpp)
+	Posso usar de referencia caso queira fazer alguma mais complexo.
+
+- [Pen thicknesses customization](https://github.com/xournalpp/xournalpp/discussions/4920)
+	Caso eu queria mexer com pen thicknesses customizada é preciso recompilar o programa.
+
+- [Função `app.msgbox` foi descontinuada, substituida por `app.openDialog`.](https://github.com/xournalpp/xournalpp/discussions/5100)
+
+###Para o template de pagina
+- [Issue: options to adjust format of paper background](https://github.com/xournalpp/xournalpp/issues/2137#issuecomment-799956788)
+	Todas configurações (ou quase todas) do pagetemplate comentadas pelo Rolandlo.
+
+###Para a toolbar
+- As barras de ferramentas podem ser configuradas escrevendo no arquivo **toolbar.ini** ou graficamente, dentro do aplicativo. O próprio arquivo **toolbar.ini** possui todas as funções comentadas, mas é recomendável utilizá-lo caso ocorra algum bug, já que a configuração direta no aplicativo é mais fácil. Lembre-se de que a janela de personalização da barra de ferramentas utiliza a paleta de cores **palette.gpl**, logo, é melhor que esse arquivo esteja na pasta antes de configurar a barra de ferramentas.
+
+###Outros sites usados
+- [Cor HEX pra RGB.](https://www.rapidtables.com/convert/color/hex-to-rgb.html "Cor HEX pra RGB")
+- [Color Picker do google.](https://g.co/kgs/ufCdTF "Color Picker do google")
+- [Converter pt to inches e vice e versa.](https://www.convertunits.com/from/pt/to/inch "Converter pt to inches")
+- [Keyboard Shortcut Map Maker.](https://archie-adams.github.io/keyboard-shortcut-map-maker/)
+
+##To-do
+- ...
---
+++ main.lua
@@ -0,0 +1,268 @@
+--KeyToLearn plugin, for 1.2.0 stable version of Xournal++
+
+--Hex values to my colors                   -- RGB          (order of usage, kinda)
+local GraphiteColor   = 0x292929            -- 41 41 41
+local blueColor       = 0x1982c4            -- 25 130 196
+local purpleColor     = 0x5B0888  --Novo    -- 91 8 136
+local greenColor      = 0x86DA3B  --Novo    -- 134 218 59
+local yellowColor     = 0xFFCA3A            -- 255 202 58
+local redColor        = 0xC53737            -- 197 55 55
+local orangeColor     = 0xFF9041  --Novo    -- 255 144 65
+local offWhiteColor   = 0xF8F8F2            -- 248 248 242
+local fullBlack       = 0x000000            -- 0 0 0
+local whiteColor      = 0xFFFFFF            -- 255 255 255
+local lightBlueColor  = 0x83b5d1  --Novo    -- 131 181 209
+local lightPurpleColor= 0x935D96  --Novo    -- 147 93 150
+local brownColor      = 0xA9794D  --Novo    -- 169 121 77
+
+--Same colors but softer sufix-S
+local blueColorS        = 0x3091cf          -- 48 145 207
+local purpleColorS      = 0x671e8f          -- 103 30 143
+local greenColorS       = 0x98e058          -- 152 224 88
+local yellowColorS      = 0xffd257          -- 255 210 87
+local redColorS         = 0xd14343          -- 209 67 67
+local orangeColorS      = 0xfc9a53          -- 252 154 83
+
+--Fonts 
+local fontname1 = "Times New Roman Regular"
+local fontname2 = "Strawberry Muffins Demo Regular"
+local fontsize1 = "6" --Similar to my FINE size writing, works for these two fonts above
+local fontsize2 = "5" --Similar to my VERY_FINE size writing
+
+--Other variables
+--local linestyleList = {"PLAIN", "DASH", "DASH_DOT", "DOT"}
+--local sizeList = {"VERY_FINE","FINE","MEDIUM", "THICK","VERY_THICK"}
+--local drawingtypeList = {"TOOL_DRAW_RECT", "TOOL_DRAW_CIRCLE", "TOOL_DRAW_ARROW", "RULER", "TOOL_DRAW_SPLINE", "SHAPE_RECOGNIZER"} -- Don't include coordinate system and default tool
+--local eraserList = {"STANDARD", "DELETE_STROKE","WHITEOUT"}
+
+function initUi()
+    --MAIN TOOLS
+    app.registerUi({["menu"] = "Pen",               ["callback"] = "clickPen",              ["accelerator"] = "q"});
+    app.registerUi({["menu"] = "Highlighter",       ["callback"] = "clickHighlighter",      ["accelerator"] = "w"});
+    app.registerUi({["menu"] = "Eraser",            ["callback"] = "clickEraser",           ["accelerator"] = "e"});
+    app.registerUi({["menu"] = "Hand",              ["callback"] = "clickHand",             ["accelerator"] = "x"});
+    app.registerUi({["menu"] = "Text",              ["callback"] = "clickText",             ["accelerator"] = "t"});
+    app.registerUi({["menu"] = "TEX",               ["callback"] = "clickTex",              ["accelerator"] = "<Shift>t"});
+
+    --ACTION SIZES
+    app.registerUi({["menu"] = "Size Very Fine",    ["callback"] = "clickVeryFine",         ["accelerator"] = "a"});
+    app.registerUi({["menu"] = "Size Fine",         ["callback"] = "clickFine",             ["accelerator"] = "s"});
+    app.registerUi({["menu"] = "Size Medium",       ["callback"] = "clickMedium",           ["accelerator"] = "d"});
+
+    --LINE STYLES
+    app.registerUi({["menu"] = "Linestyle",         ["callback"] = "linestyle",             ["accelerator"] = "f"});
+    app.registerUi({["menu"] = "Fill mode",         ["callback"] = "clickFill",             ["accelerator"] = "<Shift>f"});
+    app.registerUi({["menu"] = "Cycle Eraser Style",["callback"] = "eraserstyle",           ["accelerator"] = "<Shift>e"});
+
+    --SHAPES
+    app.registerUi({["menu"] = "Shape Recognizer",  ["callback"] = "clickShapeRecognizer",  ["accelerator"] = "c"});
+    app.registerUi({["menu"] = "Ruler/Line",        ["callback"] = "clickRuler",            ["accelerator"] = "r"});
+    app.registerUi({["menu"] = "Draw Rectangle",    ["callback"] = "clickRectangle",        ["accelerator"] = "<Shift>a"});
+    app.registerUi({["menu"] = "Draw Ellipse",      ["callback"] = "clickEllipse",          ["accelerator"] = "<Shift>d"});
+    app.registerUi({["menu"] = "Draw Spline",       ["callback"] = "clickSpline",           ["accelerator"] = "<Shift>s"});
+    --app.registerUi({["menu"] = "Cycle Drawing Type", ["callback"] = "drawingType", ["accelerator"] = "c"});
+
+    --SELECTIONS
+    app.registerUi({["menu"] = "Select Rectangle",  ["callback"] = "clickSelectRectangle",  ["accelerator"] = "g"});
+    app.registerUi({["menu"] = "Select Region",     ["callback"] = "clickSelectRegion",     ["accelerator"] = "<Shift>g"});
+
+    --ZOOM AND PAGES
+    app.registerUi({["menu"] = "Zoom In (MesaDig)", ["callback"] = "clickZoomIn",           ["accelerator"] = "<Shift>i"});     --Botão de cima da caneta
+    app.registerUi({["menu"] = "Zoom Out (MesaDig)",["callback"] = "clickZoomOut",          ["accelerator"] = "<Shift>o"});     --Botão de baixo da caneta
+    app.registerUi({["menu"] = "Zoom Fit ",         ["callback"] = "clickZoomFit",          ["accelerator"] = "<Shift>x"});
+    --Configurar no driver da Wacom pra quando tiver Xournal++ aberto ser Shift+i no botão de cima e Shift+o no debaixo
+
+    --OTHER FUNCTIONS
+    app.registerUi({["menu"] = "Clean All",         ["callback"] = "cleanAll",              ["accelerator"] = "<Shift>q"});
+    app.registerUi({["menu"] = "Grid Snap",         ["callback"] = "gridSnap",              ["accelerator"] = "v"});
+    app.registerUi({["menu"] = "Angle Snap",        ["callback"] = "rotationSnap",          ["accelerator"] = "<Shift>v"});
+end
+
+---------------------------------------
+--            MAIN TOOLS             --
+---------------------------------------
+
+function clickPen()
+    app.uiAction({["action"] = "ACTION_TOOL_PEN"})
+end
+
+function clickEraser()
+    app.uiAction({["action"] = "ACTION_TOOL_ERASER"})
+    app.uiAction({["action"] = "ACTION_SIZE_VERY_FINE"})    --Só por que é muito raro eu aumentar o tamanho dela
+    cleanShapes()
+    cleanLinestyle()
+end
+
+function clickHighlighter()
+    app.uiAction({["action"] = "ACTION_TOOL_HIGHLIGHTER"})
+end
+
+function clickHand()
+    app.uiAction({["action"]="ACTION_TOOL_HAND"})
+    cleanShapes()
+end
+
+function clickText()
+    app.uiAction({["action"]="ACTION_TOOL_TEXT"})
+end
+
+function clickTex()
+    app.uiAction({["action"] = "ACTION_TEX"})
+end
+
+---------------------------------------
+--           ACTION SIZES            --
+---------------------------------------
+
+function clickVeryFine()
+    app.uiAction({["action"] = "ACTION_SIZE_VERY_FINE"})
+  end
+
+function clickFine()
+    app.uiAction({["action"] = "ACTION_SIZE_FINE"})
+end
+
+function clickMedium()
+    app.uiAction({["action"] = "ACTION_SIZE_MEDIUM"})
+end
+
+---------------------------------------
+--            LINE STYLES            --
+---------------------------------------
+
+local linestyleList = {"PLAIN", "DASH", "DASH_DOT", "DOT"}
+local currentLinestyle = 1
+
+function linestyle()
+    currentLinestyle = currentLinestyle % #linestyleList + 1
+    app.uiAction({["action"] = "ACTION_TOOL_LINE_STYLE_" .. linestyleList[currentLinestyle]})
+end
+
+local currentFill = false
+function clickFill()
+    currentFill = not currentFill
+    app.uiAction({["action"]="ACTION_TOOL_FILL", ["enabled"] = currentFill})
+end
+
+local eraserList = {"STANDARD", "DELETE_STROKE"} --I don't use WHITEOUT normally
+local currentEraser = 1
+
+function eraserstyle()
+    currentEraser = currentEraser % #eraserList + 1
+    app.uiAction({["action"] = "ACTION_TOOL_ERASER_" .. eraserList[currentEraser]})
+end
+
+---------------------------------------
+--               SHAPES              --
+---------------------------------------
+
+local drawingTypeList = {"TOOL_DRAW_RECT", "TOOL_DRAW_ELLIPSE", "TOOL_DRAW_SPLINE"}     -- Don't include coordinate system and default tool
+local currentDrawingType = 1
+
+local currentShapeRecognizer = true
+function clickShapeRecognizer()
+    currentShapeRecognizer = not currentShapeRecognizer
+    app.uiAction({["action"] = "ACTION_SHAPE_RECOGNIZER", ["enabled"] = currentShapeRecognizer})
+end
+
+local currentRuler = false
+function clickRuler()
+    currentRuler = not currentRuler
+    app.uiAction({["action"] = "ACTION_RULER", ["enabled"] = currentRuler})
+end
+
+--[[ function drawingType()
+    currentDrawingType = currentDrawingType % #drawingTypeList + 1
+    app.uiAction({["action"] = "ACTION_" .. drawingTypeList[currentDrawingType]})
+    print("ACTION_" .. drawingTypeList[currentDrawingType])
+end ]]
+
+local currentRectangle = false
+function clickRectangle()
+    currentRectangle = not currentRectangle
+    app.uiAction({["action"] = "ACTION_TOOL_DRAW_RECT", ["enabled"] = currentRectangle})
+end
+
+local currentEllipse = false
+function clickEllipse()
+    currentEllipse = not currentEllipse
+    app.uiAction({["action"] = "ACTION_TOOL_DRAW_ELLIPSE", ["enabled"] = currentEllipse})
+end
+
+local currentSpline = false
+function clickSpline()
+    currentSpline = not currentSpline
+    app.uiAction({["action"] = "ACTION_TOOL_DRAW_SPLINE", ["enabled"] = currentSpline})
+end
+
+---------------------------------------
+--             SELECTIONS            --
+---------------------------------------
+
+function clickSelectRectangle()
+    app.uiAction({["action"] = "ACTION_TOOL_SELECT_RECT"})
+end
+
+function clickSelectRegion()
+    app.uiAction({["action"] = "ACTION_TOOL_SELECT_REGION"})
+end
+
+---------------------------------------
+--          ZOOM AND PAGES           --
+---------------------------------------
+
+function clickZoomIn()
+    app.uiAction({["action"] = "ACTION_ZOOM_IN"})
+end
+
+function clickZoomOut()
+    app.uiAction({["action"] = "ACTION_ZOOM_OUT"})
+end
+
+function clickZoomFit()
+    app.uiAction({["action"]="ACTION_ZOOM_FIT"})
+end
+  
+---------------------------------------
+--          OTHER FUNCTIONS          --
+---------------------------------------
+
+function cleanAll()
+    --Fazer um for loop aqui pra ele limpar o highlighter, pen, e borracha, dá pra fazer com os '..' só vai mudar o EX: ACTION_TOOL_{NOME}_DRAW_ARROW
+    --N precisa fazer esse loop, como ela vai limpar da ferramenta que está selecionada, logo se eu quiser limpar o 'Highlighter' eu clico 'w' e dps '<Shift>q'
+    app.uiAction({["action"] = "ACTION_RULER", ["enabled"] = false})
+    app.uiAction({["action"] = "ACTION_TOOL_DRAW_ARROW", ["enabled"] = false})
+    app.uiAction({["action"] = "ACTION_TOOL_DRAW_RECT", ["enabled"] = false})
+    app.uiAction({["action"] = "ACTION_TOOL_DRAW_ELLIPSE", ["enabled"] = false})
+    app.uiAction({["action"] = "ACTION_TOOL_DRAW_SPLINE", ["enabled"] = false})
+    app.uiAction({["action"] = "ACTION_TOOL_FILL", ["selection"] = true, ["enabled"] = false})
+    app.uiAction({["action"] = "ACTION_TOOL_SHAPE_RECOGNIZER", ["enabled"] = false})
+
+    app.uiAction({["action"] = "ACTION_TOOL_LINE_STYLE_PLAIN"})
+    app.uiAction({["action"] = "ACTION_TOOL_PEN_SHAPE_RECOGNIZER", ["enabled"] = true})
+    app.uiAction({["action"] = "ACTION_TOOL_PEN_SIZE_FINE"})
+    app.uiAction({["action"] = "ACTION_TOOL_ERASER_SIZE_VERY_FINE"})
+    app.uiAction({["action"] = "ACTION_TOOL_ERASER_DELETE_STROKE"})
+end
+
+function cleanShapes() --Used in other functions, not as shortcuts
+    app.uiAction({["action"] = "ACTION_RULER", ["enabled"] = false})
+    app.uiAction({["action"] = "ACTION_TOOL_DRAW_ARROW", ["enabled"] = false})
+    app.uiAction({["action"] = "ACTION_TOOL_DRAW_RECT", ["enabled"] = false})
+    app.uiAction({["action"] = "ACTION_TOOL_DRAW_ELLIPSE", ["enabled"] = false})
+    app.uiAction({["action"] = "ACTION_TOOL_DRAW_SPLINE", ["enabled"] = false})
+    app.uiAction({["action"] = "ACTION_TOOL_SHAPE_RECOGNIZER", ["enabled"] = false})
+end
+
+function cleanLinestyle() --Used in other functions, not as shortcuts
+    app.uiAction({["action"] = "ACTION_TOOL_LINE_STYLE_PLAIN"})
+    app.uiAction({["action"] = "ACTION_TOOL_FILL", ["enabled"] = false})
+end
+
+function gridSnap()
+    app.uiAction({["action"]="ACTION_GRID_SNAPPING"})
+end
+
+function rotationSnap()
+    app.uiAction({["action"]="ACTION_ROTATION_SNAPPING"})
+end
